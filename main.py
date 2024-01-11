5a9b14e2537c05c046c10be7d018b09f80f198f7
reserved79 <50192158+reserved79@users.noreply.github.com>
2024-01-11 16:06:01 +0800

    main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ from ocs_cb import ocs_cb
 if __name__ == '__main__':
     data = read_data()
     X, Y, X_train, X_validation, Y_train, Y_validation = split_data(data)
-    # tra_ml_ass(X_train, Y_train)
+    tra_ml_ass(X_train, Y_train)
     ocs_cb(X_train, X_validation, Y_train, Y_validation)
-    # val_cb_ass(X_train, X_validation, Y_train, Y_validation)
+    val_cb_ass(X_train, X_validation, Y_train, Y_validation)
